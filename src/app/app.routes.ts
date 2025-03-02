cf6e7dd8dfce0e7523c75ad318f4b100c0a43156
Jose Miguel <jmpg.dev@gmail.com>
2025-03-02 13:44:35 +0100

    feature: Theme Configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,23 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { AboutComponent } from './pages/about/about.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { StatsComponent } from './pages/stats/stats.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
-    { path: 'main', component: MainComponent },
-    { path: '', redirectTo: 'main', pathMatch: 'full' }, // Redirige correctamente a 'main'
-    { path: '**', redirectTo: 'main' } // Captura cualquier ruta desconocida y la envía a 'main'
+    { path: '', component: MainComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'resume', component: ResumeComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'skills', component: SkillsComponent },
+    { path: 'stats', component: StatsComponent },
+    { path: 'testimonials', component: TestimonialsComponent },
+    { path: '**', redirectTo: '' }
 ];
--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-skills',
   imports: [],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
